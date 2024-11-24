3006e370cea1b3e84356ca42ae98e3459295dbf2
YangGwangSeong <rhkdtjd_12@naver.com>
2024-11-24 18:37:32 +0900

    :sparkles: feat: 모집글 수정 api 추가 & 모집글 삭제 api 추가 (#35)

--- src/controllers/articles.controller.ts
+++ src/controllers/articles.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Delete,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post,
+} from "@nestjs/common";
 
 import { CurrentMemberDecorator } from "@APP/common/decorators/current-member.decorator";
 import { CreateArticleDto } from "@APP/dtos/create-article.dto";
+import { UpdateArticleDto } from "@APP/dtos/update-article.dto";
 import { ArticlesService } from "@APP/services/articles.service";
 
 @Controller("articles")
@@ -14,5 +23,26 @@ export class ArticlesController {
         @CurrentMemberDecorator("id") currentMemberId: number,
     ) {
         return this.articlesService.createArticle(currentMemberId, body);
+    }
+
+    @Patch(":articleId")
+    async patchArticle(
+        @Param("articleId", new ParseIntPipe()) articleId: number,
+        @Body() body: UpdateArticleDto,
+        @CurrentMemberDecorator("id") currentMemberId: number,
+    ) {
+        return this.articlesService.updateById(
+            articleId,
+            currentMemberId,
+            body,
+        );
+    }
+
+    @Delete(":articleId")
+    async deleteArticle(
+        @Param("articleId", new ParseIntPipe()) articleId: number,
+        @CurrentMemberDecorator("id") currentMemberId: number,
+    ) {
+        return this.articlesService.deleteById(articleId, currentMemberId);
     }
 }
