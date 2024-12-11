8c7845ba8757dbdaa9e4435efcae3c35d7d36afb
YangGwangSeong <rhkdtjd_12@naver.com>
2024-12-12 08:39:01 +0900

    :sparkles: feat: data-source 파일 작성 및 script 추가 (#54)

---
+++ data-source.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
+import { DataSource } from "typeorm";
+
+import {
+    ENV_DB_DATABASE,
+    ENV_DB_HOST,
+    ENV_DB_PASSWORD,
+    ENV_DB_PORT,
+    ENV_DB_TYPE,
+    ENV_DB_USERNAME,
+} from "@APP/common/constants/env-keys.const";
+
+dotenv.config({
+    path: path.resolve(
+        process.cwd(),
+        process.env["NODE_ENV"] === "production"
+            ? ".env"
+            : process.env["NODE_ENV"] === "stage"
+              ? ".stage.env"
+              : ".development.env",
+    ),
+});
+
+export default new DataSource({
+    type: (process.env[ENV_DB_TYPE] as any) || "mysql",
+    host: process.env[ENV_DB_HOST] || "localhost",
+    port: Number(process.env[ENV_DB_PORT]) || 3306,
+    username: process.env[ENV_DB_USERNAME] || "root",
+    password: process.env[ENV_DB_PASSWORD] || "test",
+    database: process.env[ENV_DB_DATABASE] || "test",
+    entities: [path.resolve(process.cwd(), "dist/**/*.entity.{js,ts}")],
+});
