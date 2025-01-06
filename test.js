ea4c05536be3a5d953d58d52662b669ea3fd22a5
YangGwangSeong <rhkdtjd_12@naver.com>
2025-01-06 22:57:37 +0900

    :sparkles: feat: db포트 넘버타입 설정 (#76)

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT),
 });
 
 const server = http.createServer((req, res) => {
